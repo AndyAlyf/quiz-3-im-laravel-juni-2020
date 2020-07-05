0abd482c44b9d5aaf83a699c14c4b71515ea291d
Andi Alifsyach <andyalyfsyah4@gmail.com>
2020-07-05 19:12:35 +0800

    step 2 : make mingration

---
+++ database/migrations/2020_07_05_110417_create_article_table.php
@@ -0,0 +1,36 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateArticleTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('article', function (Blueprint $table) {
+            $table->bigIncrements('id');
+            $table->string('judul',45);
+            $table->string('isi',500);
+            $table->string('slug',45);
+            $table->string('tag',45);
+            $table->unsignedBigInteger('penulis_id');
+            $table->foreign('penulis_id')->references('id_user')->on('user');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('article');
+    }
+}
